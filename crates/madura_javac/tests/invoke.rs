4c36812603141d874878747614a067992bbfde3d
Sam Gammon <sam@elide.dev>
2026-08-07 13:19:45 -0700

    feat: elide-based local crate build
    Signed-off-by: Sam Gammon <sam@elide.dev>

---
+++ crates/madura_javac/tests/invoke.rs
@@ -0,0 +1,8 @@
+use std::ffi::OsString;
+
+#[test]
+fn rejects_interior_nul_arguments() {
+    let err = madura_javac::invoke([OsString::from("Foo\0.java")])
+        .expect_err("interior NUL must be rejected before reaching FFI");
+    assert_eq!(err.arg, OsString::from("Foo\0.java"));
+}
