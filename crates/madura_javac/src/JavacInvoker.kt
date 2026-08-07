4f9fdffefd62cc847f944c25c9893f0aacdaf283
Sam Gammon <sam@elide.dev>
2026-08-07 12:40:35 -0700

    feat(madura_javac): invoke the system java compiler with passthrough args

--- crates/madura_javac/src/JavacInvoker.kt
+++ crates/madura_javac/src/JavacInvoker.kt
@@ -1,27 +1,16 @@
 package dev.elide.jvm;
 
-import javax.tools.JavaCompiler
 import javax.tools.ToolProvider
-import javax.tools.JavaFileObject
-import javax.tools.DiagnosticCollector
-import java.io.File
 
 object JvmInvoker {
-  //
   @JvmStatic fun main(args: Array<String>) {
-    // compiler
     val compiler = ToolProvider.getSystemJavaCompiler()
     if (compiler == null) {
-      System.out.println("Failed to load compiler.")
+      System.err.println("madura: system Java compiler is not available in this image")
       System.exit(2)
       return
     }
-
-    // diag
-    val diagnostics = DiagnosticCollector<JavaFileObject>()
-
-    // exit
-    System.out.println("Hello Kotlin Entry")
-    System.exit(0)
+    System.exit(compiler.run(System.`in`, System.out, System.err, *args))
   }
 }
+
